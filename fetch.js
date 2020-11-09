8961c4e8ecc18750cfb77c2979f1d18693b93586
Andrew Vlasov <vlasov.andrii@gmail.com>
2020-11-09 01:05:29 +0100

    fetchMore function added

--- fetch.js
+++ fetch.js
@@ -1,6 +1,8 @@
 
    
-        
+         var i;
+
+         //-----------this is initial fetch----------//
         fetch('https://my-json-server.typicode.com/TomaszJaworski/test-api/news')
             .then(response=>{
                 return response.json();
@@ -9,14 +11,42 @@
             })
             .then(data =>{
                console.log(data);
-               data.forEach(myFunction); // iterating data as an array
-               
-                // document.getElementById("demo").innerHTML = txt;
+               console.log(data.length)
+              // data.forEach(myFunction); // iterating data as an array
+              
+                for (i = 0; i < 5; i++) {
+                    myFunction(0,i,data)
+                }
+                console.log(`i=`+i);
             })
-            
-            // var txt = "";
 
-            function myFunction(value, index, array) {
+
+
+            fetchmore = function(){
+                fetch('https://my-json-server.typicode.com/TomaszJaworski/test-api/news')
+                .then(response=>{
+                    return response.json();
+                })
+                .then(data =>{
+                console.log(data.length)
+                if (i>=data.length){console.log('No More Data!')}
+                else{
+                let p=i+2
+                    for (i ; i < p; i++) {
+                        
+                        myFunction(0,i,data)
+                    }
+                    console.log(`i=`+i);
+                }
+                })
+               
+
+            }
+            
+
+            myFunction = function (value, index, array) {
+                console.log(`myFunction i=`+i);
+                console.log(`index =`+index);
                 // txt = txt + "index: "+index +"<br>"+"title: "+ array[index].title+"<br>";
                 //  console.log(array.length);
                 
@@ -42,13 +72,13 @@
                 document.getElementById("div_and_q"+index).appendChild(node);
 
 
-                 //-----------this adds h2 to the new div-----------//
+                 //-----------this adds h2 to the new div_and_q-----------//
                 var node = document.createElement("h2");
                 var textnode = document.createTextNode(array[index].title);
                 node.appendChild(textnode);
                 document.getElementById("div_and_q"+index).appendChild(node);
               
-                //-----------this adds h2 to the myList div-----------//
+                //-----------this adds h2 to the div_and_q-----------//
 
                  //-----------this adds date to the new div-----------//
                  var node = document.createElement("text");
@@ -59,26 +89,20 @@
                 document.getElementById("Div"+index).appendChild(node);
                 //-----------this adds p to the myList div-----------//
 
-                 //-----------this adds p to the new div-----------//
-                 var node = document.createElement("p");
-                 node.setAttribute("class", "newsText");
-                var textnode = document.createTextNode(array[index].text);
-                node.appendChild(textnode);
-                document.getElementById("Div"+index).appendChild(node);
-                //-----------this adds p to the myList div-----------//
+               
                
                 //-----------this adds img to the new div-----------//
                 var img = document.createElement('img');
                 img.setAttribute("class", "newsImg");
                 img.src = array[index].image;
                 document.getElementById("Div"+index).appendChild(img);
+
+                  //-----------this adds p to the new div-----------//
+                  var node = document.createElement("p");
+                  node.setAttribute("class", "newsText");
+                 var textnode = document.createTextNode(array[index].text);
+                 node.appendChild(textnode);
+                 document.getElementById("Div"+index).appendChild(node);
+                 //-----------this adds p to the myList div-----------//
                 
-
-                
-
-
-
-
-            
-
             }
