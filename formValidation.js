8f9fa19651c053a2a595361b2545a5a77aa80f34
Andrew Vlasov <vlasov.andrii@gmail.com>
2020-11-10 15:00:05 +0100

    form falidation for Logowanie.html page

---
+++ formValidation.js
@@ -0,0 +1,7 @@
+const name = document.getElementById('log_login');
+const pass = document.getElementById('log_password');
+const form = document.getElementById('logowanie_form');
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+})
