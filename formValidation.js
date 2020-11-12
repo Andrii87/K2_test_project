3cf1ae4379b0a97ca9d445edc4df6fe8065f3346
Andrew Vlasov <vlasov.andrii@gmail.com>
2020-11-12 21:08:02 +0100

    toggle switch and custom checkbox -perfect version

--- formValidation.js
+++ formValidation.js
@@ -1,7 +1,11 @@
 const name = document.getElementById('log_login');
 const pass = document.getElementById('log_password');
 const form = document.getElementById('logowanie_form');
+// const form = document.getElementById('logowanie_form');
+// const form = document.getElementById('logowanie_form');
 
 form.addEventListener('submit', (e) =>{
+    let messages = []
+    if (name.value ===''||){}
     e.preventDefault()
 })
